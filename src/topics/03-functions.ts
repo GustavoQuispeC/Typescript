25192ba268c0fa5810039c5589b715a5946676e8
Gustavo <gusstavocta@gmail.com>
2025-12-28 00:16:53 -0500

    revisando funciones

---
+++ src/topics/03-functions.ts
@@ -0,0 +1,49 @@
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+const result: number = addNumbers(10, 15);
+console.log("Result of addition:", result);
+
+//! Arrow function version
+const addNumbersArrow = (a: number, b: number): number => a + b;
+
+const resultArrow: number = addNumbersArrow(20, 25);
+console.log("Result of arrow addition:", resultArrow);
+
+//! Function with optional and default parameters
+function multiply(
+  firstNumber: number,
+  secondNumber?: number,
+  base: number = 2
+): number {
+  return firstNumber * base;
+}
+const multiplyResult = multiply(5);
+console.log("Result of multiplication:", multiplyResult);
+
+//! Using an interface with a function
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number): void => {
+  character.hp += amount;
+  console.log(
+    `${character.name} healed by ${amount}. Current HP: ${character.hp}`
+  );
+};
+
+healCharacter(
+  {
+    name: "Gandalf",
+    hp: 50,
+    showHp() {
+      console.log(`HP: ${this.hp}`);
+    },
+  },
+  20
+);
+
+export {};
