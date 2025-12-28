d1a020891d741dcb83176dfc3c3973437e994ee6
Gustavo <gusstavocta@gmail.com>
2025-12-27 22:55:58 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+// import './topics/-basic-types'
+import './topics/02-object-interface'
+
+
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+app.innerHTML = `Hola Mundo`
---
+++ src/topics/-basic-types.ts
@@ -0,0 +1,9 @@
+const name: string = 'Gustavo';
+
+let hpPoints: number | string = 95;
+hpPoints = 'FULL';
+let isAlive: boolean = true;
+
+console.log({name, hpPoints, isAlive});
+
+export {};
